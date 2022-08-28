eb60236be49b839167e025e54954fc68d9ebadf6
Jose Nieto <jose.nieto@inglesparatodos.edu.co>
2022-08-28 10:13:09 -0500

    changes in main page

--- src/app/wanime/main-pag/main-pag.component.ts
+++ src/app/wanime/main-pag/main-pag.component.ts
@@ -10,13 +10,44 @@ import { SeriesAnime } from '../interfaces/animes.interface';
 export class MainPagComponent {
 
   // seriesAnime :  SeriesAnime[] = [];
-  
-  nuevo: SeriesAnime = {
-    nombre: 'Horimiya',
-    temporada: 2
+
+  private _seriesAnime: SeriesAnime[] = [
+    {
+      nombre: 'Violet Evergarden',
+      temporada: 2,
+    },
+    {
+      nombre: 'Demon Slayer',
+      temporada: 3,
+    },
+    {
+      nombre: 'Darwins Game',
+      temporada: 1,
+    },
+    {
+      nombre: 'Fruits Basket',
+      temporada: 2,
+    },
+    {
+      nombre: 'Sono Bisque Doll',
+      temporada: 1,
+    },
+  ];
+
+  newSerie: SeriesAnime = {
+    nombre: '',
+    temporada: 0
   };
   constructor(){
     // this.seriesAnime = this.wanimeservices.seriesAnime;
+  }
+
+  agregar(){
+    if (this.newSerie.nombre.trim().length ===0) {
+      return
+    }
+    console.log(this.newSerie);
+
   }
 
 }
--- src/app/wanime/services/wanine.service.ts
+++ src/app/wanime/services/wanine.service.ts
@@ -1,44 +1,40 @@
-import { Injectable } from "@angular/core";
-import { SeriesAnime } from "../interfaces/animes.interface";
+import { Injectable } from '@angular/core';
+import { SeriesAnime } from '../interfaces/animes.interface';
 
 @Injectable()
+export class Wanimeservice {
+  private _seriesAnime: SeriesAnime[] = [
+    {
+      nombre: 'Violet Evergarden',
+      temporada: 2,
+    },
+    {
+      nombre: 'Demon Slayer',
+      temporada: 3,
+    },
+    {
+      nombre: 'Darwins Game',
+      temporada: 1,
+    },
+    {
+      nombre: 'Fruits Basket',
+      temporada: 2,
+    },
+    {
+      nombre: 'Sono Bisque Doll',
+      temporada: 1,
+    },
+  ];
 
-export class Wanimeservice{
+  get seriesAnime(): SeriesAnime[] {
+    return [...this._seriesAnime];
+  }
 
-   private _seriesAnime:SeriesAnime[] = [
-        { 
-          nombre:'Violet Evergarden',
-          temporada: 2
-        },
-        { 
-          nombre:'Demon Slayer',
-          temporada: 3
-        },
-        { 
-          nombre:'Darwins Game',
-          temporada: 1
-        },
-        { 
-          nombre:'Fruits Basket',
-          temporada: 2
-        },
-        { 
-          nombre:'Sono Bisque Doll',
-          temporada: 1
-        }
-        ];
+  constructor() {
+    console.log('Servicio inicializado');
+  }
 
-    get seriesAnime(): SeriesAnime[]{
-        return [...this._seriesAnime];
-    }
-   
-   constructor(){
-       console.log('Servicio inicializado');
-   }
-
-   addSeries(serie:SeriesAnime){
-       this._seriesAnime.push(serie);
-   }
-    
-
+  addSeries(serie: SeriesAnime) {
+    this._seriesAnime.push(serie);
+  }
 }
